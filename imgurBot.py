20b2d693a3af9d1202f06a6b1f615a1fd25c61fb
Steven Lay <stlay@ucsd.edu>
2017-09-30 09:26:00 -0700

    removed deleted comments from showing up and added a limit for comments

--- imgurBot.py
+++ imgurBot.py
@@ -7,7 +7,9 @@ reddit = praw.Reddit("imgurAlbum")
 submission = reddit.submission(url="%s" % sys.argv[1])
 submission.comment_sort = "top"
 submission.comments.replace_more(limit=0)
+submission.comment_limit = 10
 
 for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+    if top_level_comment.body != "[deleted]":
+        print(top_level_comment.body)
 
