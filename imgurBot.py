bb5b7f385fa6be49182dafc22d29586d4c0a094c
Steven Lay <stlay@ucsd.edu>
2017-10-06 15:16:33 -0700

    extract imgur lilnks from comments

--- imgurBot.py
+++ imgurBot.py
@@ -7,9 +7,15 @@ reddit = praw.Reddit("imgurAlbum")
 submission = reddit.submission(url="%s" % sys.argv[1])
 submission.comment_sort = "top"
 submission.comments.replace_more(limit=0)
-submission.comment_limit = 10
+LIMIT = 5
+COUNTER = 0
 
 for top_level_comment in submission.comments:
+    if COUNTER == LIMIT:
+        break
     if top_level_comment.body != "[deleted]":
-        print(top_level_comment.body)
+       comment_text = top_level_comment.body_html
+       link = comment_text[comment_text.index("href=\"") + 6:comment_text.rindex("\">")]
+       print((link))
+    COUNTER += 1
 
