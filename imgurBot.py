7a70cf926629ed684bce5b6de50b348fef24f954
Steven Lay <stlay@ucsd.edu>
2017-10-06 15:41:01 -0700

    parsing of description in comment

--- imgurBot.py
+++ imgurBot.py
@@ -1,24 +1,34 @@
 import praw
 import sys
-
-
+LIMIT = 5
+HREF_SIZE = 6
+BUFFER_SIZE = 2
 #bot
 reddit = praw.Reddit("imgurAlbum")
 submission = reddit.submission(url="%s" % sys.argv[1])
 submission.comment_sort = "top"
 submission.comments.replace_more(limit=0)
-LIMIT = 5
-COUNTER = 0
 
+COUNTER = 0
 for top_level_comment in submission.comments:
+   
+    #stop once comment is reached
     if COUNTER == LIMIT:
         break
     if top_level_comment.body != "[deleted]":
        comment_text = top_level_comment.body_html
-       #link = comment_text[comment_text.index("href=\"") + 6:comment_text.rindex("\">")]
-       start = comment_text.find("href=\"") + 6
-       end = comment_text.find("\">", start)
-       link = comment_text[start:end]
-       print((link))
+      
+       #get image link posted
+       image_start = comment_text.find("href=\"") + HREF_SIZE
+       image_end = comment_text.find("\">", image_start)
+       link = comment_text[image_start:image_end]
+       print(link)
+       
+       #get description posted
+       description_start = comment_text.find(link) + len(link) + BUFFER_SIZE
+       description_end = comment_text.find("</a", description_start)
+       description = comment_text[description_start:description_end]
+       print(description)
+    
     COUNTER += 1
 
